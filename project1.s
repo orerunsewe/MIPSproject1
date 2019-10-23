55e6bf98ca7e8dad96c1d696683dac1247f5d29f
Ore <ore_runsewe@outlook.com>
2019-10-23 10:37:31 -0400

    Preallocated space to read user's input string and included syscall instructions to read the user's input string appropriately.

--- project1.s
+++ project1.s
@@ -1,1 +1,9 @@
+.data
+        input_str: .space   11        # Preallocate space for 10 characters and the null string
 
+.text
+        main:
+        li $v0, 8                     # Instruction to get user's input string
+        la $a0, input_str             # Load register with address of input string
+        li $a1, 10                    # Read maximum of 10 characters from string (i.e. do not read null character)
+        syscall     
