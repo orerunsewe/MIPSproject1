e852cf9de41dfe9ce088eb04aad7635e12573d5a
Ore <ore_runsewe@outlook.com>
2019-10-25 01:44:03 -0400

    Edited contents of Loop1 and started working on subroutine for conversion

--- project1.s
+++ project1.s
@@ -9,9 +9,23 @@
             syscall
 
             la $s0, input_str             # Load register with address of input string
-            add $a2, $zero, $zero         # Initialize the counter to equal 0
+            add $t0, $zero, $zero         # Initialize the counter to equal 0
 
             Loop1:
-                add $t0, $a2, $s0         # add counter to input address and store in $t0 to get current character
-                lb $t1, 0($t0)            # Get current character in the string
-                bne $t1, 0, Convert       # If the current character is not null, it is not the end of file. Go to Convert
+                add $t1, $t0, $s0         # Add counter to input address and store in $t1 to get the current character's address
+                lb $a2, 0($t1)            # Get current character in the string
+                jal ConvertToDecimal      # Jump to subroutine to get decimal value of the current character
+
+
+            # This subroutine is used to convert the string characters to their corresponding decimal values, treating each character as a base-N number
+            # Conversions done based on formula N = 26 + (X % 11) where X is my StudentID: 02805400
+            # N = 30 so valid range is from 'a' to 't' or 'A' to 'T'
+            # Characters '0' to '9' correspond to a decimal value of 0 to 9 respectively
+            # Characters 'a' to 't' correspond to a decimal value of 10 to 29 respectively
+            # Characters 'A' to 'T' correspond to a decimal value of 10 to 29 respectively
+            # All other characters are out of range and correspond to a decimal value 0
+            # Register $a2 contains current character in the string
+
+            ConvertToDecimal:
+                add $t2, $zero, $a2       # Copy character at $a2 to temporary register $t2
+                addi $t3, $zero, 87       # Load $t3 with reference value 87 (ascii value of 'a' - 10) for conversion
